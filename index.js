d3f0e78348ba6591bb0218c2c2d7004eb85fb59a
github username <elis.paul7@gmail.com>
2018-11-08 22:29:45 -0500

    sort function

---
+++ index.js
@@ -0,0 +1,30 @@
+const _ = require('lodash');
+const request = require('request');
+const express = require('express');
+const app = express();
+
+// Constants
+const SHEET_URL = 'https://sheets.googleapis.com/v4/spreadsheets/1i2qbKeasPptIrY1PkFVjbHSrLtKEPIIwES6m2l2Mdd8/values/A1%3AE45?key=AIzaSyDKy_NLpawUj6ylQ5-ylXVKajMevNV9byo';
+const ASCEND_SORT = 'asc';
+const DESCEND_SORT = 'desc';
+
+app.use('/presidents', (req, res) => {
+  const sortOrder = req.query.sort === DESCEND_SORT ? DESCEND_SORT : ASCEND_SORT;
+  console.log(sortOrder, req.query);
+
+  request(
+    SHEET_URL,
+    (err, response, body) => {
+      const parsedBody = JSON.parse(body);
+      const { values } = parsedBody;
+
+      const [columnNames, ...presidentData] = values;
+      const presidentDataWithColumnHash = _.map(presidentData, v => _.zipObject(columnNames, v));
+      const sortedPresidentData = _.orderBy(presidentDataWithColumnHash, ['President'], [sortOrder]);
+
+      res.send(sortedPresidentData);
+    },
+  );
+});
+
+app.listen(3000);
